51a539e9daf70cb32eb02cdee0be65be2fe6f5f4
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-06-17 00:16:03 +0800

    Update project_health_monitoring_system.ino

--- project_health_monitoring_system.ino
+++ project_health_monitoring_system.ino
@@ -1,10 +1,31 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
-#include <AHT10.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+#include "DHT.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
+
+MAX30105 particleSensor;
+
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
 
 float valueRoomTemp = 0;
 float valueBodyTemp = 0;
@@ -18,6 +39,12 @@ String ValueRoomHum;
 String ValueHeartRate;
 String ValueSpo2;
 
+#define DHTPIN 2 
+
+#define DHTTYPE DHT11  
+
+DHT dht(DHTPIN, DHTTYPE);
+
 #define ONE_WIRE_BUS 4
 
 // Setup a oneWire instance to communicate with any OneWire device
@@ -26,12 +53,6 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
-AHT10             myAHT10(AHT10_ADDRESS_0X38);
-
-#define REPORTING_PERIOD_MS     1000
- 
-PulseOximeter pox;
-uint32_t tsLastReport = 0;
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
@@ -47,31 +68,28 @@ unsigned long lastTime = 0;
 // Set timer to 5 seconds (5000)
 unsigned long timerDelay = 5000;
 
-void onBeatDetected()
-{
-    Serial.println("Beat!");
-}
-
 void setup() {
   Serial.begin(115200);
-
-  if (!pox.begin()) {
-        Serial.println("FAILED");
-        for(;;);
-    } else {
-        Serial.println("SUCCESS");
-    }
-   pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
- 
-    // Register a callback for the beat detection
-   pox.setOnBeatDetectedCallback(onBeatDetected);
-
-  /* AHT10 connection check */
-  while (myAHT10.begin() != true)
+  dht.begin();
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println(F("AHT10 not connected or fail to load calibration coefficient"));
-    delay(5000);
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
   }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 
   sensors.begin();  // Start up the library
 
@@ -90,21 +108,11 @@ void setup() {
 
 void loop() {
   sensors.requestTemperatures(); 
-  pox.update();
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        Serial.print("Heart rate:");
-        Serial.print(pox.getHeartRate());
-        Serial.print("bpm / SpO2:");
-        Serial.print(pox.getSpO2());
-        Serial.println("%");
- 
-        tsLastReport = millis();
-    }
     
-  valueHeartRate = pox.getHeartRate();
-  valueSpo2 = pox.getSpO2();
-  valueRoomTemp = myAHT10.readTemperature(AHT10_FORCE_READ_DATA);
-  valueRoomHum = myAHT10.readHumidity(AHT10_USE_READ_DATA);
+  valueHeartRate = heartRate;
+  valueSpo2 = spo2;
+  valueRoomTemp = dht.readTemperature();
+  valueRoomHum = dht.readHumidity();
   valueBodyTemp = sensors.getTempCByIndex(0);
 
   ValueRoomTemp = String(valueRoomTemp);
@@ -115,6 +123,72 @@ void loop() {
 
   PostData();
 }
+
+void sensormax (){
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  }
+  }
 
 
 void PostData(){
@@ -127,7 +201,6 @@ void PostData(){
     
       // Your Domain name with URL path or IP address with path
       http.begin(client, serverName);
-
       
       // If you need an HTTP request with a content type: application/json, use the following:
       http.addHeader("Content-Type", "application/json");
